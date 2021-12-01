0af9bf9bb37a1559480cce3609cac724c20056a1
kenan <kenan.husic@stud.fh-campuswien.ac.at>
2021-12-01 11:20:06 +0100

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,30 @@
 public class Main {
     public static void main(String[] args) {
 
-        
+        // hiii Leute wilkommbe bei spilndsnslkd Spielregeln Handbuch
+
+        //Spieler name 1
+
+        //Spieler name 2
+        //Spieler... make your move
+
+
+        // 2D Array
+
+        // Counter++ Rot Spalte 1,2,3..
+
+        // Counter++ Grün 1,2,3..
+
+        // if Counter mit den einer Farbe auf 4
+
+        // if alle felder belegt -- kein spieler hat gewonnen
+
+        // Reset button falls kein sieg mehr möglich
+
+        // Spieler R/G Gewonnen
+
+        // Neues Spiel
+
+
     }
 }
